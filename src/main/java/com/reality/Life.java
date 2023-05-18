71e3cb0375320a0ff8c2b2fc3037eda5200dcc2c
Akshith <vangarusaiakshith98@gmail.com>
2023-05-18 19:23:01 +0530

    Instead of Chamma Chekka modified to 'My World!' & Chamma to 'Your World!'

--- src/main/java/com/reality/Life.java
+++ src/main/java/com/reality/Life.java
@@ -5,12 +5,12 @@ public class Life {
     private Health wellBeing;
     private Girlfriend wifeMaterial;
     private Children hyperActive;
-//    private Wife caringAndCrazy;
+    private Wife caringAndCrazy;
     private Dog forHappiness;
     private Home aPlaceOfLove;
     private Friend notManyButWhomYouCanTrust;*/
 
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+        System.out.println("My World!");
     }
 }
