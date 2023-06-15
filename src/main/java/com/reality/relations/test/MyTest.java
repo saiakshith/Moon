86513309cc90d8dd5e1e21e589dfce1f0611a3c2
Akshith <saiakshith@nendrasys.com>
2023-06-15 21:03:15 +0530

    Add new file MyTest.java.

---
+++ src/main/java/com/reality/relations/test/MyTest.java
@@ -0,0 +1,17 @@
+package com.reality.relations.test;
+
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("hello world 1");
+
+        System.out.println("Special line by akshith.");
+        System.out.println("hello world 2");
+
+        System.out.println("hello world 3");
+        System.out.println("hello world 4");
+
+        System.out.println("Line 2 added by akshith");
+
+        System.out.println("hello world 5");
+    }
+}
