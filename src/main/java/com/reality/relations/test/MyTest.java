66e740662913fead2c5a64ed341148d1b8b50a27
Akshith <mahendra@nendrasys.com>
2023-06-26 20:29:00 +0530

    Update LifeTest, MyTest, progress.txt

--- src/main/java/com/reality/relations/test/MyTest.java
+++ src/main/java/com/reality/relations/test/MyTest.java
@@ -2,7 +2,7 @@ package com.reality.relations.test;
 
 public class MyTest {
     public static void main(String[] args) {
-        System.out.println("hello world 1");
+        System.out.println("hello world 1, this is to create a conflict in feature/akshith/tests-creating-conflicts branch, while merging release/enh-tests");
 
         System.out.println("Commit single file 2");
     }
