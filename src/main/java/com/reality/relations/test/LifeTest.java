977d93ad7b3539e42f6d259016e42c4c0cad4777
saiakshith <75861046+saiakshith@users.noreply.github.com>
2023-06-27 00:11:12 +0530

    Update LifeTest.java
    Update LifeTest.java

--- src/main/java/com/reality/relations/test/LifeTest.java
+++ src/main/java/com/reality/relations/test/LifeTest.java
@@ -7,7 +7,7 @@ public class LifeTest {
 
         System.out.println("Print statement added, FROM INTELLIJ");
 
-        System.out.println("Print statement added, FROM GITHUB");
+        System.out.println("Print statement added, FROM GITHUB, another change FROM GITHUB");
 
         System.out.println("Commit single file 1");
     }
