2b9b64c98caf060cde96a57d9e181fe44eb8edb8
Akshith <saiakshith@nendrasys.com>
2023-07-03 19:28:14 +0530

    Update LifeTest.java, FROM INTELLIJ

--- src/main/java/com/reality/relations/test/LifeTest.java
+++ src/main/java/com/reality/relations/test/LifeTest.java
@@ -3,16 +3,7 @@ package com.reality.relations.test;
 public class LifeTest {
     public static void main(String[] args) {
 
-        System.out.println("This line is added from local in intellij by akshith");
+        System.out.println("New line added, FROM INTELLIJ");
 
-        System.out.println("Print statement added, FROM INTELLIJ");
-
-        System.out.println("Print statement added, FROM GITHUB, another change FORM INTELLIJ");
-
-        System.out.println("Print statement added, FROM GITHUB, ANOTHER CONFLICT FROM GITHUB");
-
-        System.out.println("Commit single file 1");
-
-        System.out.println("NEW LINE WITH NO CONFLICT FROM GITHUB");
     }
 }
