684d9331d5b6fcf3bfae9a817191318ad133e0f9
saiakshith <75861046+saiakshith@users.noreply.github.com>
2023-07-03 19:21:42 +0530

    Update LifeTest.java FROM GITHUB
    Update LifeTest.java FROM GITHUB

--- src/main/java/com/reality/relations/test/LifeTest.java
+++ src/main/java/com/reality/relations/test/LifeTest.java
@@ -3,10 +3,7 @@ package com.reality.relations.test;
 public class LifeTest {
     public static void main(String[] args) {
 
-        System.out.println("This line is added from local in intellij by akshith");
+        System.out.println("New line added, FROM GITHUB");
 
-        System.out.println("Line added in, release/enh-tests, commit-1");
-
-        System.out.println("Line added in, release/enh-tests, commit-2");
     }
 }
