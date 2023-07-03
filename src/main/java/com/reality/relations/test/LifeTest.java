8720e164b6c747965ee0ea555732d4739daf678f
ramarao66 <132896457+ramarao66@users.noreply.github.com>
2023-07-03 20:50:51 +0530

    Update LifeTest.java, FROM GITHUB
    Update LifeTest.java, FROM GITHUB

--- src/main/java/com/reality/relations/test/LifeTest.java
+++ src/main/java/com/reality/relations/test/LifeTest.java
@@ -3,7 +3,7 @@ package com.reality.relations.test;
 public class LifeTest {
     public static void main(String[] args) {
 
-        System.out.println("New line added, FROM GITHUB, this addition is to create conflict");
+        System.out.println("New line added, FROM GITHUB, this addition is to create conflict FROM GITHUB");
 
     }
 }
