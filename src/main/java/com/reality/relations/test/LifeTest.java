5056d32bce6e9101cf632323e5105cd42c7f08b9
saiakshith <vangarusaiakshith98@gmail.com>
2023-07-04 16:05:08 +0530

    Update LifeTest.java, FROM INTELLIJ

--- src/main/java/com/reality/relations/test/LifeTest.java
+++ src/main/java/com/reality/relations/test/LifeTest.java
@@ -4,14 +4,11 @@ public class LifeTest {
     public static void main(String[] args) {
 
 
+
+
+//        Impex for NotificationContent & EmailPage
         System.out.println("New line added, FROM INTELLIJ");
 
-        System.out.println("New line added, FROM GITHUB");
-
-        System.out.println("New line added, FROM GITHUB, this addition is to create conflict FROM GITHUB");
-
-        System.out.println("New line added, FROM GITHUB, this addition is to create conflict FROM GITHUB");
-
-        System.out.println("New line added, FROM GITHUB, this addition is to create conflict FROM GITHUB");
+        System.out.println("New line added, FROM INTELLIJ");
     }
 }
