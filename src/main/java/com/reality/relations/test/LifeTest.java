379f632a6e1ed5e4eb4e33ff5ce5f11deaf73862
saiakshith <vangarusaiakshith98@gmail.com>
2023-07-05 19:51:24 +0530

    Update Life Test.java

--- src/main/java/com/reality/relations/test/LifeTest.java
+++ src/main/java/com/reality/relations/test/LifeTest.java
@@ -3,11 +3,10 @@ package com.reality.relations.test;
 public class LifeTest {
     public static void main(String[] args) {
 
+// some code 
 
-
-//        Impex for NotificationContent & EmailPage
-        System.out.println("New line added, FROM INTELLIJ");
-
+        System.out.println("New line of code");
+        
         System.out.println("New line added, FROM INTELLIJ");
     }
 }
