010041b612f2dcf0a064d2e114f28722cff6a7f0
Akshith Testing <vangarusaiakshith98@gmail.com>
2023-07-17 17:55:33 +0530

    Update pom.xml & LifeTest.java

--- src/main/java/com/reality/relations/test/LifeTest.java
+++ src/main/java/com/reality/relations/test/LifeTest.java
@@ -1,12 +1,20 @@
 package com.reality.relations.test;
 
+import org.apache.commons.codec.binary.StringUtils;
+import org.apache.commons.lang.BooleanUtils;
+
+import javax.sql.rowset.serial.SQLOutputImpl;
+import java.util.HashMap;
+import java.util.Map;
+
 public class LifeTest {
     public static void main(String[] args) {
-
-// some code 
-
-        System.out.println("New line of code");
         
-        System.out.println("New line added, FROM INTELLIJ");
+        Map<String, String> map = new HashMap<>();
+        map.put("me", "y");
+        
+        
+        System.out.println(BooleanUtils.toBoolean(map.get("me")));
     }
+    
 }
--- src/main/java/com/reality/relations/test/MyTest.java
+++ src/main/java/com/reality/relations/test/MyTest.java
@@ -3,6 +3,8 @@ package com.reality.relations.test;
 public class MyTest {
     public static void main(String[] args) {
 
+        System.out.println("New line, FROM INTELLIJ");
+        
         System.out.println("Yet another line FROM GITHUB WITHOUT CONFLICT");
 
         System.out.println("New line, FROM GITHUB");
