350e03b7539855642ab71be87aef03653e55b60f
Akshith-MT-98 <vangarusaiakshith98@gmail.com>
2023-07-18 19:18:32 +0530

    Update EmailAppNotificationService.java

--- src/main/java/com/reality/relations/firebase/EmailAppNotificationService.java
+++ src/main/java/com/reality/relations/firebase/EmailAppNotificationService.java
@@ -1,4 +1,8 @@
 package com.reality.relations.firebase;
 
 public class EmailAppNotificationService {
+    public static void main(String[] args) {
+        System.out.println("A new line of code from, INTELLIJ");
+        
+    }
 }
