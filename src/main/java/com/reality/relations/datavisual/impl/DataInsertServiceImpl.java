aa441601c8935abdf1d050a7404a0acf3bd897ff
Akshith-MT-98 <vangarusaiakshith98@gmail.com>
2023-07-19 16:44:04 +0530

    Revert unwanted changes

--- src/main/java/com/reality/relations/datavisual/impl/DataInsertServiceImpl.java
+++ src/main/java/com/reality/relations/datavisual/impl/DataInsertServiceImpl.java
@@ -5,7 +5,6 @@ import com.reality.relations.datavisual.DataInsertService;
 public class DataInsertServiceImpl implements DataInsertService {
     @Override
     public void getdata() {
-        System.out.println("Akshith");
-        System.out.println("Akshith 2");
+
     }
 }
