11ad7b72fbfc1209fc74550fbfff443f3969f36b
Akshith-MT-98 <vangarusaiakshith98@gmail.com>
2023-07-31 12:11:11 +0530

    Add StreamsEx7.java -> FlatMap

---
+++ src/main/java/com/enh/streams/exercises/StreamsEx7.java
@@ -0,0 +1,31 @@
+package com.enh.streams.exercises;
+
+import com.enh.streams.StreamUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+//    FlatMap
+public class StreamsEx7 {
+
+    public static void main(String[] args) {
+//    a. Given a list of lists of integers, flatten it into a single list of integers.
+        System.out.println(
+                StreamUtils.getOrderedIntegers().stream()
+                        .map(i -> List.of(i + 1, i - 1))
+                        .flatMap(li -> li.stream())
+//                        .filter(i -> i >= 0)
+//                        .sorted()
+                        .collect(toList())
+        );
+        
+//    b. Convert a list of sentences into a list of words using flatMap.
+        System.out.println(
+                StreamUtils.getSentencesList().stream()
+                        .flatMap(s -> Arrays.stream(s.split(" ")))
+                        .collect(toList())
+        );
+    }
+}
