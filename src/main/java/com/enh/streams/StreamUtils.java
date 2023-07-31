c1843a1a5902e77fddb748d119dc1a4424e73bff
Akshith-MT-98 <vangarusaiakshith98@gmail.com>
2023-07-31 13:36:21 +0530

    Add intermediate StreamsEx1.java -> Filtering & Transformation

--- src/main/java/com/enh/streams/StreamUtils.java
+++ src/main/java/com/enh/streams/StreamUtils.java
@@ -89,16 +89,16 @@ public class StreamUtils {
     public static List<Product> getProducts() {
         return List.of(
                 new Product("SG-22-101", "Samsung Galaxy S22", "Buy galaxy & change your life", new BigDecimal(17_000), true, Arrival.OLD),
-                new Product("TV-22-475", "Samsung TV 2", "Buy samsung tv & change your life", new BigDecimal(2_31_000), true, Arrival.OLD),
+                new Product("TV-22-475", "Samsung TV 2", "Buy samsung tv & change your life", new BigDecimal(2_31_000), false, Arrival.OLD),
                 new Product("SGU-23-613", "Samsung S23 Ultra", "Buy galaxy ultra S23 & change your life", new BigDecimal(58_000), true, Arrival.NEW),
-                new Product("SG-23-213", "Samsung Galaxy S23", "Buy samsung S23 & change your life", new BigDecimal(23_000), true, Arrival.OLD),
+                new Product("SG-23-213", "Samsung Galaxy S23", "Buy samsung S23 & change your life", new BigDecimal(23_000), false, Arrival.OLD),
                 new Product("ST-22-875", "Samsung Tab 2", "Buy samsung tab & change your life", new BigDecimal(78_000), true, Arrival.NEW),
                 new Product("STI-22-913", "Samsung Trade-In", "Buy samsung trade-in & change your life", new BigDecimal(9_000), true, Arrival.NEW),
-                new Product("ST-22-101", "Samsung Theft", "Buy samsung tab & change your life", new BigDecimal(58_000), true, Arrival.OLD),
+                new Product("ST-22-101", "Samsung Theft", "Buy samsung tab & change your life", new BigDecimal(58_000), false, Arrival.OLD),
                 new Product("TV-22-095", "Samsung TV 1", "Buy samsung tv & change your life", new BigDecimal(1_10_000), true, Arrival.NEW),
-                new Product("ST-22-774", "Samsung Tab 1", "Buy samsung theft & change your life", new BigDecimal(5_000), true, Arrival.OLD),
+                new Product("ST-22-774", "Samsung Tab 1", "Buy samsung theft & change your life", new BigDecimal(5_000), false, Arrival.OLD),
                 new Product("SM-22-565", "Samsung Monitor 1", "Buy samsung monitor & change your life", new BigDecimal(1_45000), true, Arrival.NEW),
-                new Product("SC-22-312", "Samsung Care Plus", "Buy samsung care plus & change your life", new BigDecimal(2_000), true, Arrival.NEW)
+                new Product("SC-22-312", "Samsung Care Plus", "Buy samsung care plus & change your life", new BigDecimal(2_000), false, Arrival.NEW)
         );
     }
 }
