dbbc52e2b97e9f5121a4003453baca2bc603e3fe
Akshith-MT-98 <vangarusaiakshith98@gmail.com>
2023-08-20 16:14:41 +0530

    Add java classes & interfaces practicals

---
+++ src/main/java/com/enh/java/classes_interfaces/ClassA.java
@@ -0,0 +1,22 @@
+package com.enh.java.classes_interfaces;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ClassA extends ClassB {
+
+    @Override
+//    protected String returnStringWithParams(String s, int i) {
+    public String returnStringWithParams(String s, int i) {
+        return s.substring(i);
+    }
+    
+    // Overriding
+//    public char returnStringWithParams(String s, String i) {
+//    public char returnStringWithParams(String s, int i) {
+//    protected char returnStringWithParams(int s, String i) {
+//        return s.charAt(i);
+//        return s.charAt(0);
+//        return i.charAt(s);
+//    }
+}
