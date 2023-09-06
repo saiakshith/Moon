7d48ce11282527f5abcfac37e8c1c468a405d71b
Prudhvi <narsingprudhvi231@gmail.com>
2023-09-06 15:02:42 +0530

    updating StreamsEx1 & created StreamsTest

--- src/main/java/com/enh/streams/exercises/prudhvi/intermediate/StreamsEx1.java
+++ src/main/java/com/enh/streams/exercises/prudhvi/intermediate/StreamsEx1.java
@@ -7,8 +7,8 @@ import java.util.stream.Collectors;
 
 public class StreamsEx1 {
 
-//    FlatMap:
     public static void main(String[] args) {
+//    FlatMap:
 //    a. Given a list of lists of integers, flatten it into a single list and print the result.
         System.out.println(
                 StreamUtils.getOrderedIntegers().stream()
@@ -22,5 +22,52 @@ public class StreamsEx1 {
                         .distinct()
                         .collect(Collectors.toList())
         );
+//        Parallel Streams:
+//        a. Given a large list of integers, find the sum of all the numbers using parallel streams and compare the performance with sequential streams.
+        long startTime = System.currentTimeMillis();
+        System.out.println(
+                StreamUtils.getOrderedNumbers().parallelStream()
+                        .mapToInt(Integer::intValue)
+                        .sum()
+        );
+        long endTime = System.currentTimeMillis();
+        System.out.println("Parallel Time : " + (endTime - startTime) + "ms");
+
+        startTime = System.currentTimeMillis();
+        System.out.println(
+                StreamUtils.getOrderedNumbers().stream()
+                        .sequential()
+                        .mapToInt(Integer::intValue)
+                        .sum()
+        );
+        endTime = System.currentTimeMillis();
+        System.out.println("Sequential : " + (endTime - startTime) + "ms");
+
+//        b. Given a list of strings, perform an expensive operation on each element in parallel and print the time taken for both parallel and sequential processing.
+        startTime = System.currentTimeMillis();
+        StreamUtils.strings().parallelStream()
+                        .forEach(s -> {
+                            try {
+                                Thread.sleep(1);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+        endTime = System.currentTimeMillis();
+        System.out.println("Parallel string time : " + (endTime - startTime) + "ms");
+
+        startTime = System.currentTimeMillis();
+        StreamUtils.strings().parallelStream()
+                .sequential()
+                .forEach(s -> {
+                            try {
+                                Thread.sleep(1);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+        endTime = System.currentTimeMillis();
+        System.out.println("Sequential string time : " + (endTime - startTime) + "ms");
+
     }
 }
