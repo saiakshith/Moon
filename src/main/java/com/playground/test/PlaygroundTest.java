17cd8590f4622fc72f01a1cddfc6a2d26e43bd0e
Akshith-MT-98 <vangarusaiakshith98@gmail.com>
2024-07-08 08:26:37 +0530

    Refactor PlaygroundTest.java

--- src/main/java/com/playground/test/PlaygroundTest.java
+++ src/main/java/com/playground/test/PlaygroundTest.java
@@ -1,4 +1,4 @@
-package com.playground;
+package com.playground.test;
 
 
 import java.lang.reflect.Array;
@@ -17,14 +17,6 @@ public class PlaygroundTest {
     }
 
     public static void main(String[] args) {
-
-        List<String> superList = List.of("sai", "ravi", "akhil");
-        List<String> subList1 = List.of("sai", "ravi");
-        List<String> subList2 = List.of("sai", "veera");
-
-        Map<String, String> map = new HashMap<>();
-        map.put("key", "value");
-        map.put("name", "person");
         
         Instant instant = Instant.now();
         
@@ -33,6 +25,3 @@ public class PlaygroundTest {
     }
 }
 
-class PlaygroundTest2 {
-    
-}
